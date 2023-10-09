4b7d436ce47a84e5ca0ed29ec2141860e79da834
daxnet <sunny.chen@perkinelmer.com>
2023-10-09 16:34:43 +0800

    Refactoring the side panel.

---
+++ src/web/Models/NoteData.cs
@@ -0,0 +1,11 @@
+﻿namespace CloudNotes.Web.Models
+{
+    public class NoteData
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public DateTime DateCreatedUtc { get; set; }
+        public DateTime? DateModifiedUtc { get; set; }
+    }
+}
