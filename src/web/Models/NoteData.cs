29360526336c3c08e2f32de20d0e3dda04f1805b
daxnet <daxnet@outlook.com>
2023-10-28 21:31:02 +0800

    Integrated tinymce, implemented creating notes.

--- src/web/Models/NoteData.cs
+++ src/web/Models/NoteData.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Globalization;
+using System.Text.Json.Serialization;
 
 namespace CloudNotes.Web.Models
 {
@@ -18,5 +19,27 @@ namespace CloudNotes.Web.Models
         
         [JsonPropertyName("dateModified")]
         public DateTime? DateModifiedUtc { get; set; }
+
+        [JsonIgnore]
+        public string DateCreatedRepresentation
+        {
+            get
+            {
+                if (DateTime.UtcNow - DateCreatedUtc < TimeSpan.FromMinutes(1))
+                {
+                    return "刚刚";
+                }
+                else if (DateTime.UtcNow - DateCreatedUtc < TimeSpan.FromDays(1))
+                {
+                    return $"今天 {DateCreatedUtc.ToLocalTime().ToShortTimeString()}";
+                }
+                else if (DateTime.UtcNow - DateCreatedUtc < TimeSpan.FromDays(2))
+                {
+                    return $"昨天 {DateCreatedUtc.ToLocalTime().ToShortTimeString()}";
+                }
+
+                return DateCreatedUtc.ToLocalTime().ToString(CultureInfo.CurrentCulture);
+            }
+        }
     }
 }
--- src/web/config.sh
+++ src/web/config.sh
@@ -1,2 +1,2 @@
-﻿echo Replacing environment variables
-sed -i "s#__SERVICE_URL__#$SERVICE_URL#g" /usr/share/nginx/html/appsettings.Production.json
+﻿#!/bin/sh
+sed -i "s#__SERVICE_URL__#$SERVICE_URL#g" "/usr/share/nginx/html/appsettings.Production.json"
